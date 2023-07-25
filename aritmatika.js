507cb6ea443127866fd31b1721c680ea00940e66
fisabilla <140366526+fisabilla@users.noreply.github.com>
2023-07-25 15:04:36 +0700

    aritmatika.js

---
+++ aritmatika.js
@@ -0,0 +1,14 @@
+let bil1 = 8;
+    let bil2 = 7;
+   
+let pertambahan=bil1+bil2;
+let pengurangan=bil1-bil2;
+let perkalian=bil1*bil2;
+let pembagian=bil1/bil2;
+let modulus=bil1%bil2;
+     
+document.write("Hasil penambahan"+ pertambahan+"<br>");
+document.write("Hasil pengurangan"+pengurangan+"<br>");
+document.write("Hasil perkalian"+perkalian+"<br>");
+document.write("Hasil pembagian"+ pembagian+"<br>");
+document.write("Hasil modulus"+ modulus+"<br>");
